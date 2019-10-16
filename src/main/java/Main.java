ba6c1175325f014dc7c314603b155f19258a418c
Damian <d.pietrucuzk@gmail.com>
2019-10-16 12:06:24 +0200

    adding scanner and use for class IsPrimary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        double number = sc.nextInt();
+        if(IsPrimary.isPrimary(number)){
+            System.out.println((int)number + " jest liczbą pierwszą");
+        } else {
+            System.out.println((int)number + " nie jest liczbą pierwszą");
+        }
     }
 }
