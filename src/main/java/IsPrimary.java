a12fcf4935969df198bbd2aecb3688a0ec50491c
Damian <d.pietrucuzk@gmail.com>
2019-10-16 12:03:51 +0200

    adding class IsPrimary

---
+++ src/main/java/IsPrimary.java
@@ -0,0 +1,13 @@
+public class IsPrimary {
+    public static boolean isPrimary(double number){
+        int count=0;
+        for (double i =1; i<=Math.sqrt(number); i++){
+            if (count>1){
+                return Boolean.FALSE;
+            }
+            if (number%i==0)
+                count++;
+        }
+        return Boolean.TRUE;
+    }
+}
