bb8f270153b81608fc1b9e152bb56bfc07ba71e1
Damian <d.pietrucuzk@gmail.com>
2019-10-18 11:45:45 +0200

    add MainApp class and fix isPrimary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double number = sc.nextInt();
-        if(IsPrimary.isPrimary(number)){
-            System.out.println((int)number + " jest liczbą pierwszą");
-        } else {
-            System.out.println((int)number + " nie jest liczbą pierwszą");
-        }
-        System.out.println(Factorial.factorial((int)number));
+        MainApp.mainApp();
     }
 }
