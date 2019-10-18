0eaf4cc40572a6a73b01fd3d8f2b140a77cec557
Damian <d.pietrucuzk@gmail.com>
2019-10-18 13:37:08 +0200

    zmiany2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Zmiany");
+        System.out.println("Zmiany123");
         Scanner sc = new Scanner(System.in);
         double number = sc.nextInt();
         if(IsPrimary.isPrimary(number)){
